dbe57f0ef4ef83230fb08e8e3d6bd904fca96136
Jörg Kantel <joerg@kantel.de>
2018-07-23 17:21:42 +0200

    MayaVi publiziert

--- sketches/mayavitest.py
+++ sketches/mayavitest.py
@@ -6,6 +6,11 @@ import numpy as np
 from mayavi import mlab
 from scipy.special import sph_harm
 
+# Mit diesem Werten kann gespielt werden:
+l = 3
+m = 0
+# --------------------------------------
+
 theta_ld = np.linspace(0, np.pi, 91)
 phi_ld = np.linspace(0, 2*np.pi, 181)
 
@@ -13,9 +18,6 @@ theta_2d, phi_2d = np.meshgrid(theta_ld, phi_ld)
 xyz_2d = np.array([np.sin(theta_2d)*np.sin(phi_2d),
                    np.sin(theta_2d) * np.cos(phi_2d),
                    np.cos(theta_2d)])
-
-l = 3
-m = 0
 
 Y_lm = sph_harm(m, l, phi_2d, theta_2d)
 r = abs(Y_lm.real)*xyz_2d
