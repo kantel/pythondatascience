b9fedea06b1d3535db4020b58bd5a73b14ce38ff
Jörg Kantel <joerg@kantel.de>
2018-08-01 18:02:01 +0200

    Image-IO

---
+++ sketches/image.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import os
+
+# Hier wird der Pfad zum Verzeichnis des ».py«-Files gesetzt
+# Erspart einem das Herumgehample in TextMate mit dem os.getcwd()
+# und os.path.join()
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+
+
+im = Image.open("images/rotgelbblau.jpg")
+im.show()
